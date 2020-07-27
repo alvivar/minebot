2705852924254b552fef42b4df015acd06d583a1
Andrés Villalobos <andresalvivar@gmail.com>
2020-07-27 10:32:33 -0600

    Improved

--- src/main.rs
+++ src/main.rs
@@ -3,73 +3,79 @@ use std::process::Command;
 fn main() {
     // Reading the log
 
-    let log_command =
+    let logcommand =
         Command::new("docker")
                 .args(&["logs", "--tail", "100", "mc"])
                 .output()
                 .expect("docker logs failed");
 
-    let log = String::from_utf8(log_command.stdout).unwrap();
+    let log = String::from_utf8(logcommand.stdout).unwrap();
     let lines: Vec<&str> = log.split("\n").collect();
 
     for line in lines.into_iter() {
+
+        // Analyzing
+
         let len = line.len();
         let start_bytes = line.find('<').unwrap_or(0);
         let end_bytes = line.find('>').unwrap_or(0);
 
-        let username_sliced = &line[start_bytes..end_bytes];
-        let username: String = username_sliced.chars().skip(1).collect();
+        let user_slice = &line[start_bytes..end_bytes];
+        let user: String = user_slice.chars().skip(1).collect();
 
         let line_sliced = &line[end_bytes..len];
-        let line_content: String = line_sliced.chars().skip(2).collect();
+        let line: String = line_sliced.chars().skip(2).collect();
 
         if end_bytes > start_bytes {
-            println!("Username: {}", username);
-            println!("Command: {}", line_content);
+            println!("user: {}", user);
+            println!("command: {}", line);
         }
 
-        if line_content.starts_with("thunder to ")
+        // Reactions
+
+        let tp_keysave = "in the name of light i claim the land of ";
+        if line.starts_with(tp_keysave)
         {
-            let coords = line_content.replace("thunder to ", "");
+            let tpcommand = format!("tp {} {}", user, "~ ~ ~");
+            println!("{}", tpcommand);
+
+            Command::new("docker")
+                .args(&["exec", "-i"])
+                .args(&["mc", "rcon-cli"])
+                .arg(&tpcommand)
+                .status()
+                .expect("Failed to execute process");
+        }
+
+        let tpkey = "i summon the light to ";
+        if line.starts_with(tpkey)
+        {
+            let coords = line.replace(tpkey, "");
             println!("{}", coords);
 
-            let mut tpcoords: Vec<&str> = coords.trim().split(" ").collect();
-            let mut tpcommand = format!("tp {} {}", username, coords.trim());
+            let tpcoords: Vec<&str> = coords.trim().split(" ").collect();
+            let tpcommand = format!("tp {} {}", user, coords.trim());
             println!("{}", tpcommand);
 
             if tpcoords.len() == 3 {
-                let rcon_command =
-                    Command::new("docker")
-                        .args(&["exec", "-i"])
-                        .args(&["mc", "rcon-cli"])
-                        .arg(&tpcommand)
-                        .status()
-                        .expect("Failed to execute process");
-            }
-        }
-
-        if line_content.starts_with("please thunder a golden apple!")
-        {
-            let rcon_command =
                 Command::new("docker")
                     .args(&["exec", "-i"])
                     .args(&["mc", "rcon-cli"])
-                    .args(&["give", "adronomicon", "golden_apple"])
+                    .arg(&tpcommand)
                     .status()
                     .expect("Failed to execute process");
+            }
+        }
+
+        if line.starts_with("please thunder a golden apple!")
+        {
+            // let rcon_command =
+            //     Command::new("docker")
+            //         .args(&["exec", "-i"])
+            //         .args(&["mc", "rcon-cli"])
+            //         .args(&["give", "adronomicon", "golden_apple"])
+            //         .status()
+            //         .expect("Failed to execute process");
         }
     }
-
-    // Commands as reactions to the log
-
-    // let rcon_command =
-    //     Command::new("docker")
-    //             // .args(&["ps", "-a"])
-    //             .args(&["exec", "-i"])
-    //             .args(&["mc", "rcon-cli"])
-    //             .args(&["give", "adronomicon", "golden_apple"])
-    //             .status()
-    //             .expect("Failed to execute process");
-
-    // println!("Result: {}", rcon_command);
 }
